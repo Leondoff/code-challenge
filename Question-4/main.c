16833160f1219209a5c197c8eefa63f4d757b363
Maziyar Gerami <maziyar.grami@gmail.com>
2023-12-08 06:34:34 +0330

    Question 4

--- Question-4/main.c
+++ Question-4/main.c
@@ -1,40 +1,99 @@
 #include <stdio.h>
-#include <regex.h>
 
-int isValidString(const char *input) {
-    regex_t regex;
-    const char *pattern = "^[A-Z]*$|\\[.*\\]|\\{.*\\}|\\(.*\\)$";
 
-    if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
-        fprintf(stderr, "Error compiling regex pattern\n");
-        return 0; // Error
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+struct Stack {
+    int array[MAX_SIZE];
+    int top;
+};
+
+void initializeStack(struct Stack* stack) {
+    stack->top = -1;
+}
+
+int isEmpty(struct Stack* stack) {
+    return stack->top == -1;
+}
+
+int isFull(struct Stack* stack) {
+    return stack->top == MAX_SIZE - 1;
+}
+
+void push(struct Stack* stack, int value) {
+    if (isFull(stack))
+        return;
+
+    stack->array[++stack->top] = value;
+}
+
+int pop(struct Stack* stack) {
+    if (isEmpty(stack))
+        exit(1);
+
+    return stack->array[stack->top--];
+}
+
+int isCharInArray(char target, const char *charArray) {
+    while (*charArray != '\0') {
+        if (*charArray == target) {
+            return 1; // Character found
+        }
+        charArray++;
     }
+    return 0;
+}
 
-    int match = regexec(&regex, input, 0, NULL, 0);
+int areAllCharsInArray(const char *str, const char *charArray) {
+    while (*str != '\0') {
+        if (!isCharInArray(*str, charArray)) {
+            return 0;
+        }
+        str++;
+    }
+    return 1;
+}
 
-    regfree(&regex);
+void pushAll(struct Stack* stack, const char* str) {
+    while (*str != '\0') {
+        push(stack, *str);
+        str++;
+    }
+}
 
-    return match == 0;
+int areCharsInAscendingOrderInStack(struct Stack* stack) {
+    while (stack->top > 0) {
+        char current = pop(stack);
+        char next = pop(stack);
+
+        if (current > next) {
+            return 0; // Characters are not in ascending order
+        }
+    }
+    return 1; // Characters are in ascending order
 }
 
 int main() {
-    char input[100];
+    char validChars[] = {'(', ')', '{', '}', '[', ']', '\0'};
+    char input;
 
     printf("Enter a string: ");
-    fgets(input, sizeof(input), stdin);
+    scanf("%c", &input);
 
-    // Remove the newline character at the end (if present)
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (input[i] == '\n') {
-            input[i] = '\0';
-            break;
-        }
-    }
+    struct Stack myStack;
+    pushAll(&myStack, &input);
 
-    if (isValidString(input)) {
-        printf("The string is valid.\n");
+    int r = areCharsInAscendingOrderInStack(&myStack);
+
+    printf(">>>>>>>>>>>>>>>>>> %d",r);
+
+    if (areAllCharsInArray(&input, validChars)) {
+        printf("true\n");
     } else {
-        printf("The string is not valid.\n");
+        printf("false\n");
     }
 
     return 0;
